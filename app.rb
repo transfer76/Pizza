55adabb656b4c6016c1b91e24bca4b9f8cb3b275
transfer76 <shina76@ukr.net>
2018-11-18 21:42:20 +0200

    add Rakefile, Gemfile, add model

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizz.db"
+
+class Product < Activerecord::Base
+end	
 
 get '/' do
 	erb :index
