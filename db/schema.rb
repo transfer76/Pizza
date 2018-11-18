9c03973ab182496de0fd46350c810a4da1e579e0
transfer76 <shina76@ukr.net>
2018-11-19 00:17:53 +0200

    Migration AddProduct

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_194919) do
+ActiveRecord::Schema.define(version: 2018_11_18_210541) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
