73fafdafbc58abd6b822af9e65e7c35e21893c70
transfer76 <shina76@ukr.net>
2018-11-21 20:38:42 +0200

    Commitment app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 function something()
 {
-	var x = window.localStorage.getItem('bbb');
-	x=x*1+1;
-    window.localStorage.setItem('bbb', x); 
+	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
+	x=x*1+1; // x = x+1
+    window.localStorage.setItem('bbb', x); // hh['bbb'] = x 
     alert(x);
 }
