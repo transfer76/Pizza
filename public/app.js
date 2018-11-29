2c06dd8eb1cd4a0f6c44c417fa9765bcf6eaa018
transfer76 <shina76@ukr.net>
2018-11-29 23:51:46 +0200

    Add button

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ function something()
 
 function add_to_cart(id)
 {
-	var key = 'product_' + id
+	var key = 'product_' + id;
 	var x = window.localStorage.getItem(key);
 	x = x*1+1;
 	window.localStorage.setItem(key, x);
