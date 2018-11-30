bffa9cd52182b943b6b47f7fc89d0e02ce233f6f
transfer76 <shina76@ukr.net>
2018-11-30 22:01:29 +0200

    Add cart_get_ordes

--- public/app.js
+++ public/app.js
@@ -18,16 +18,34 @@ function add_to_cart(id)
 
 function cart_get_number_of_items()
 {
-	var cnt = 0
-	for (var i=0; i<window.localStorage.length; i++)
+	var cnt = 0;
+
+	for (var i = 0; i < window.localStorage.length; i++)
 	{
 		var key = window.localStorage.key(i);
 		var value = window.localStorage.getItem(key);
 
-		if (key.indexOf('product_')==0)
+		if (key.indexOf('product_') == 0)
 		{
-			cnt = cnt + value*1;
+			cnt = cnt + value * 1;
 		}
 	}	
 	return cnt;
+}
+
+function cart_get_orders()
+
+	var orders = '';
+
+	for (var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if (key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}	
+	return orders;
 }
