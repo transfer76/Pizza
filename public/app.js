cf9367c63e0a57a867139b712f6abd8dfddd3092
transfer76 <shina76@ukr.net>
2018-12-01 00:36:44 +0200

    update orders button

--- public/app.js
+++ public/app.js
@@ -9,19 +9,27 @@ function something()
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
+	
 	var x = window.localStorage.getItem(key);
-	x = x*1+1;
+	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
 	alert("Items in your cart: "+cart_get_number_of_items());
 
 	update_orders_input();
+	update_orders_button();
 }
 
 function update_orders_input()
 {
 	var orders = cart_get_orders();
 	$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
 }
 
 function cart_get_number_of_items()
