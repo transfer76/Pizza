8d225eb0cbd2c3749f2fc81d3e478018305450b3
transfer76 <shina76@ukr.net>
2019-06-17 23:00:12 +0300

    Add validates to Order

--- app.rb
+++ app.rb
@@ -9,7 +9,10 @@ class Product < ActiveRecord::Base
 end
 
 class Order < ActiveRecord::Base
-
+  validates :scope, presence: true
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
 end
 
 get '/' do
@@ -42,13 +45,13 @@ post '/cart' do
 end
 
 def parse_orders_input(orders_input)
-  s1 = orders_input.split(',')
+  s1 = orders_input.split(/,/)
   
   arr = []
   
   s1.each do |x|
-    s2 = x.split('=')
-    s3 = s2[0].split('_')
+    s2 = x.split(/\=/)
+    s3 = s2[0].split(/_/)
     
     id = s3[1]
     cnt = s2[1]
